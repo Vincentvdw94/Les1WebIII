7a4ff1a02990530f9bdae6739d21231a50ce16ca
Vincent Vandewyer <vincent.vandewyer@student.hogent.be>
2018-09-27 12:14:35 +0200

    creëer de klasse BankAccount

---
+++ Banking2C1/Models/Domein/BankAccount.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking2C1.Models.Domein
+{
+    public class BankAccount //default visibiliteit is internal indien niet gedefiniëerd
+    {
+
+
+
+        /* private decimal _balance; //decimal aangeraden voor geldwaarden.
+
+
+         public decimal GetBalance()
+         {
+             return _balance;
+         }
+
+         private void SetBalance(decimal value) {
+             _balance = value;
+         }*/
+
+        // public decimal Balance { get; private set; } // is het zelfde als code bovenop;
+
+
+        private decimal _balance;
+        public  const decimal WithdrawCost = 0.10M; //m is voor deciMal;
+        public decimal Balance
+        {
+            get { return _balance; }
+            set
+            {
+
+
+                if (value < 0)
+                {
+                    throw new ArgumentException("value moet boven 0");
+                }
+                _balance = value;
+
+            }
+        }
+        public void Deposit(decimal amount, int nrOfTimes = 1)//optionele parameter MOET waarde hebben, anders ist ne gewone;
+        {
+
+            Balance += amount * nrOfTimes;
+        }
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount + WithdrawCost; //gaat automatisch naar setter;
+        }
+
+        private string _accountNumber;
+        public string AccountNumber
+        {
+            get { return _accountNumber; }
+            set
+            {
+
+
+                _accountNumber = value;
+            }
+        }
+
+        #region MyRegion
+        public BankAccount(string accountNumber)
+        {
+            AccountNumber = accountNumber;
+        }
+
+        public BankAccount(string accountNumber, decimal balance) : this(accountNumber)
+        {
+            Balance = balance;
+
+        } 
+        #endregion
+
+    }
+}
--- Banking2C1/Program.cs
+++ Banking2C1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Banking2C1.Models.Domein;
+using System;
 
 namespace Banking2C1
 {
@@ -6,7 +7,17 @@ namespace Banking2C1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount myBa = new BankAccount("123-1231312-12");
+            BankAccount myBa2 = new BankAccount("123-1456874-12",1000);
+
+            Console.WriteLine($"Balance is {myBa.Balance} euro"); // $ voor de string om in de string naar variablen te refereren
+            Console.WriteLine($"Balance is {myBa2.Balance} euro"); // $ voor de string om in de string naar variablen te refereren
+            myBa2.Withdraw(50);
+            Console.WriteLine($"Balance is {myBa2.Balance} euro"); // $ voor de string om in de string naar variablen te refereren
+            myBa2.Deposit(50, 5);
+            Console.WriteLine($"Balance is {myBa2.Balance} euro"); // $ voor de string om in de string naar variablen te refereren
+
+            Console.ReadKey();
         }
     }
 }
